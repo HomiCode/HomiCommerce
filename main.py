bb68ea7bb83ecf2048551cb98ec145f736097269
Amir Homayoon <amirhomayoonrefaei@gmail.com>
2022-12-24 21:37:11 +0330

    User Registration

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 from models import *
+from authentication import get_hashed_password
+
+# Signal
+from tortoise.signals import post_save
+from typing import List, Optional, Type
+from tortoise import BaseDBAsyncClient
+
 
 app = FastAPI()
+
+
+@app.post('/registration')
+async def user_registration(user: user_pydanticIn):
+    user_info = user.dict(exclude_unset=True)
+    user_info['password'] = get_hashed_password(user_info['password'])
+    user_obj = await User.create(**user_info)
+    new_user = await user_pydantic.from_tortoise_orm(user_obj)
+
+    return {"status": "ok",
+            "data":
+                f"Hello {new_user.username} thanks for choosing our services. Please check your email inbox and click on the link to confirm your registration."}
 
 
 @app.get('/')
